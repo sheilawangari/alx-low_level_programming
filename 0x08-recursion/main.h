cf47b541f9ab8433c4942f83b97d79405c2d50c2
sheilawangari <sheilawangari@gmail.com>
2022-03-30 11:30:36 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,3 +11,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
+#endif
