eb167f58730cae35165592d12e7527d1e3eb269b
sheilawangari <sheilawangari@gmail.com>
2022-04-28 12:48:31 -0700

    A function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,79 +1,31 @@
 #include "main.h"
 
 /**
- * power - computes base raised to exp
- * @base: base number
- * @exp: exp number
- * Return: result of exponentiation
- */
-unsigned int power(int base, unsigned int exp)
-{
-	unsigned int i, result = 1;
-
-	for (i = 0; i < exp; i++)
-	result *= base;
-
-	return (result);
-}
-
-/**
- * to_decimal - converts binary string to decimal
- * @c: string to convert
- * Return: converted number
- */
-unsigned int to_decimal(const char *c)
-{
-	char *s = strdup(c);
-	unsigned int j = strlen(s) - 1, res = 0, i;
-
-	for (i = 0; s[i]; i++)
-	{
-		res += power(2, j) * (s[i] - '0');
-		j--;
-	}
-
-	return (res);
-}
-
-/**
- * binary_to_uint - converts binary number to unsigned int
- * @b: string of 0s and 1s
- * Return: unsigned int number
+ * binary_to_uint - a function that converts a binary number to an unsigned int
+ * @b: a pointer to a string of 0 and 1 chars.
+ * Return: NULL if b is NULL, 0 if b contains chars not 0 or 1.
+ * Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result, i, j = 0, flag = 0;
-	char *s = malloc(strlen(b) * sizeof(char));
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	if (s == NULL)
-		free(s);
-	
-	if (b == NULL)
-		result = 0;
+	if (*b == '\0')
+		return (0);
 
-	else
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
 	{
-		for (i = 0; b[i]; i++)
-		{
-			/* check if there's a value that isn't 0 or 1 */
-			if (b[i] != '0' && b[i] != '1')
-			{
-				result = 0;
-				flag = 1;
-				break;
-			}
-			else
-			{
-				s[j] = b[i];
-				j++;
-			}
-		}
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
-	if (s != NULL && flag == 0)
-		result = to_decimal(s);
-
-	free(s);
-	return (result);
+	return (num);
 }
 
